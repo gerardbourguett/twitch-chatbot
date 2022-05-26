cfb18abcabc84c2731293f696b3d18f55277b190
Gerard Bourguett <gerard.bourguett@gmail.com>
2022-05-26 16:50:14 -0400

    arreglo index // bot responde

--- index.js
+++ index.js
@@ -59,3 +59,33 @@ client.on("message", (channel, userstate, message, self) => {
 
   console.log(`[${badges}][${nick}] => ${message}`);
 });
+
+client.on("chat", (target, ctx, message, self) => {
+  if (self) return;
+  const commandName = message.trim();
+  if (
+    commandName === "Oli" ||
+    commandName === "oal" ||
+    commandName === "Oal" ||
+    commandName === "oli"
+  ) {
+    client.say(target, `Holi ${ctx.username}`);
+  }
+
+  if (commandName === "!dice") {
+    const num = rollDice();
+    client.say(target, `${ctx.username} rolled a ${num}`);
+  }
+
+  if (commandName === "!pancha") {
+    client.say(
+      target,
+      "vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha  "
+    );
+  }
+});
+
+function rollDice() {
+  const sides = 100;
+  const roll = Math.floor(Math.random() * sides) + 1;
+}
