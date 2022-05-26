67aea5b0c74182c096c6068ff50eb7fae4e57602
Gerard Bourguett <gerard.bourguett@gmail.com>
2022-05-26 15:02:04 -0400

    first commit

---
+++ src/bot.js
@@ -0,0 +1,57 @@
+const tmi = require("tmi.js");
+
+const options = {
+  options: {
+    debug: true,
+  },
+  connection: {
+    reconnect: true,
+  },
+  identity: {
+    username: "vanderfondi",
+    password: "oauth:71bw37rhymjc86kkdnpe77wkdvqw7o",
+  },
+  channels: ["vanderfondi"],
+};
+
+const client = new tmi.client(options);
+
+client.connect();
+
+client.on("connected", (address, port) => {
+  client.action("vanderfondi", `Soy el bot de @vanderfondi, iniciando...`);
+});
+client.on("connectFailed", function (error) {
+  console.log("Connect Error: " + error.toString());
+});
+
+client.on("chat", (target, ctx, message, self) => {
+  if (self) return;
+  const commandName = message.trim();
+  if (
+    commandName === "Oli" ||
+    commandName === "oal" ||
+    commandName === "Oal" ||
+    commandName === "oli"
+  ) {
+    client.say(target, `Holi ${ctx.username}`);
+  }
+
+  if (commandName === "!dice") {
+    const num = rollDice();
+    client.say(target, `${ctx.username} rolled a ${num}`);
+  }
+
+  if (commandName === "!pancha") {
+    const num = rollDice();
+    client.say(
+      target,
+      "vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha vander42Pancha  "
+    );
+  }
+});
+
+function rollDice() {
+  const sides = 100;
+  const roll = Math.floor(Math.random() * sides) + 1;
+}
